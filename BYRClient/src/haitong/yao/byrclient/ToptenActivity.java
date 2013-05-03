1d8666a385759ebfd6c1db72ed2b9ffe9e9f95a0
yaoyoyo <yao_frank@163.com>
2013-05-03 12:14:09 +0800

    增加帖子列表底部的功能条。

--- BYRClient/src/haitong/yao/byrclient/ToptenActivity.java
+++ BYRClient/src/haitong/yao/byrclient/ToptenActivity.java
@@ -8,6 +8,8 @@ import haitong.yao.byrclient.tasks.AbsTask;
 import haitong.yao.byrclient.tasks.GetArticlesTask;
 import haitong.yao.byrclient.tasks.ITaskFinishListener;
 import haitong.yao.byrclient.utils.BYRToast;
+import haitong.yao.byrclient.views.ListFunctionBar;
+import haitong.yao.byrclient.views.ListFunctionBar.ListBarClickListener;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
@@ -16,66 +18,94 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 public class ToptenActivity extends NoTitleActivity implements
-        OnItemClickListener, ITaskFinishListener {
+		OnItemClickListener, ITaskFinishListener, ListBarClickListener {
 
-    private final int DIVIDER_HEIGHT = 6;
+	private final int DIVIDER_HEIGHT = 6;
 
-    private ListView mToptenList;
-    private ToptenListAdapter mListAdapter;
-    private View mLoadingView;
+	private ListView mToptenList;
+	private ToptenListAdapter mListAdapter;
+	private View mLoadingView;
+	private ListFunctionBar mFunctionBar;
 
-    private Context mContext;
+	private Context mContext;
 
-    @Override
-    protected void init(Bundle savedInstanceState) {
-        setContentView(R.layout.act_topten);
-        mContext = getApplicationContext();
-        findViewsById();
-        initAdapter();
-        setListeners();
-        getArticles();
-    }
+	@Override
+	protected void init(Bundle savedInstanceState) {
+		setContentView(R.layout.act_topten);
+		mContext = getApplicationContext();
+		findViewsById();
+		initAdapter();
+		setListeners();
+		getArticles();
+	}
 
-    @Override
-    protected void findViewsById() {
-        mToptenList = (ListView) findViewById(R.id.topten_list);
-        mToptenList.setDivider(null);
-        mToptenList.setDividerHeight(DIVIDER_HEIGHT);
-        mLoadingView = findViewById(R.id.loading_view);
-    }
+	@Override
+	protected void findViewsById() {
+		mToptenList = (ListView) findViewById(R.id.topten_list);
+		mToptenList.setDivider(null);
+		mToptenList.setDividerHeight(DIVIDER_HEIGHT);
+		mLoadingView = findViewById(R.id.loading_view);
+		mFunctionBar = (ListFunctionBar) findViewById(R.id.list_function_bar);
+		mFunctionBar.setVisibility(View.GONE);
+	}
 
-    @Override
-    protected void setListeners() {
-        mToptenList.setOnItemClickListener(this);
-    }
+	@Override
+	protected void setListeners() {
+		mToptenList.setOnItemClickListener(this);
+		mFunctionBar.setBarClickListener(this);
+	}
 
-    @Override
-    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+	@Override
+	public void home() {
+		finish();
+	}
 
-    }
+	@Override
+	public void refresh() {
+		mToptenList.setVisibility(View.GONE);
+		mFunctionBar.setVisibility(View.GONE);
+		mLoadingView.setVisibility(View.VISIBLE);
+		getArticles();
+	}
 
-    @Override
-    public void onTaskFinished(AbsTask task, Object result) {
+	@Override
+	public void turnto() {
 
-        mLoadingView.setVisibility(View.GONE);
+	}
 
-        if (null == result) {
-            BYRToast.showLongToast(mContext, R.string.fail_get_content);
-        } else {
-            mToptenList.setVisibility(View.VISIBLE);
-            mListAdapter.setContent((ArrayList<Article>) result);
-            mListAdapter.notifyDataSetChanged();
-        }
+	@Override
+	public void next() {
+		BYRToast.showShortToast(mContext, R.string.notification_last_page);
+	}
 
-    }
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 
-    private void initAdapter() {
-        mListAdapter = new ToptenListAdapter(mContext);
-        mToptenList.setAdapter(mListAdapter);
-    }
+	}
 
-    private void getArticles() {
-        new GetArticlesTask(mContext, "topten", this).execute();
-    }
+	@Override
+	public void onTaskFinished(AbsTask task, Object result) {
+
+		mLoadingView.setVisibility(View.GONE);
+
+		if (null == result) {
+			BYRToast.showLongToast(mContext, R.string.fail_get_content);
+		} else {
+			mToptenList.setVisibility(View.VISIBLE);
+			mFunctionBar.setVisibility(View.VISIBLE);
+			mListAdapter.setContent((ArrayList<Article>) result);
+			mListAdapter.notifyDataSetChanged();
+		}
+
+	}
+
+	private void initAdapter() {
+		mListAdapter = new ToptenListAdapter(mContext);
+		mToptenList.setAdapter(mListAdapter);
+	}
+
+	private void getArticles() {
+		new GetArticlesTask(mContext, "topten", this).execute();
+	}
 
 }
---
+++ BYRClient/src/haitong/yao/byrclient/views/ListFunctionBar.java
@@ -0,0 +1,72 @@
+package haitong.yao.byrclient.views;
+
+import haitong.yao.byrclient.R;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.FrameLayout;
+
+public final class ListFunctionBar extends FrameLayout implements
+		OnClickListener {
+
+	private ListBarClickListener mClickListener;
+
+	public ListFunctionBar(Context context) {
+		super(context);
+		getView();
+	}
+
+	public ListFunctionBar(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		getView();
+	}
+
+	private void getView() {
+		LayoutInflater.from(getContext()).inflate(
+				R.layout.view_list_function_bar, this);
+		findViewById(R.id.list_function_bar_home).setOnClickListener(this);
+		findViewById(R.id.list_function_bar_refresh).setOnClickListener(this);
+		findViewById(R.id.list_function_bar_goto).setOnClickListener(this);
+		findViewById(R.id.list_function_bar_next_page).setOnClickListener(this);
+	}
+
+	public void setBarClickListener(ListBarClickListener listener) {
+		mClickListener = listener;
+	}
+
+	@Override
+	public void onClick(View v) {
+		int id = v.getId();
+		switch (id) {
+		case R.id.list_function_bar_home:
+			mClickListener.home();
+			break;
+		case R.id.list_function_bar_refresh:
+			mClickListener.refresh();
+			break;
+		case R.id.list_function_bar_goto:
+			mClickListener.turnto();
+			break;
+		case R.id.list_function_bar_next_page:
+			mClickListener.next();
+			break;
+		default:
+			break;
+		}
+
+	};
+
+	public interface ListBarClickListener {
+
+		public void home();
+
+		public void refresh();
+
+		public void turnto();
+
+		public void next();
+	}
+
+}
